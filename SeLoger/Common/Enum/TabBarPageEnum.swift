b6abb10e0352a0f9cf9a6cad2035f2433fa3b925
Chokri haj ayech <haj.ayech@gmail.com>
2021-03-17 11:01:41 +0100

    STYLE

--- SeLoger/Common/Enum/TabBarPageEnum.swift
+++ SeLoger/Common/Enum/TabBarPageEnum.swift
@@ -1,53 +1,53 @@
 import UIKit
 
 enum TabBarPageEnum {
-case home
-case favorites
-case settings
+	case home
+	case favorites
+	case settings
 
-init?(index: Int) {
-switch index {
-case 0:
-self = .home
-case 1:
-self = .favorites
-case 2:
-self = .settings
-default:
-return nil
-}
-}
+	init?(index: Int) {
+		switch index {
+		case 0:
+			self = .home
+		case 1:
+			self = .favorites
+		case 2:
+			self = .settings
+		default:
+			return nil
+		}
+	}
 
-func pageTitleValue() -> String {
-switch self {
-case .home:
-return "Accueil"
-case .favorites:
-return "Favoris"
-case .settings:
-return "Paramètres"
-}
-}
+	func pageTitleValue() -> String {
+		switch self {
+		case .home:
+			return "Accueil"
+		case .favorites:
+			return "Favoris"
+		case .settings:
+			return "Paramètres"
+		}
+	}
 
-func pageImageIcon() -> UIImage {
-switch self {
-case .home:
-return #imageLiteral(resourceName: "ic_home")
-case .favorites:
-return #imageLiteral(resourceName: "ic_favorite_off")
-case .settings:
-return #imageLiteral(resourceName: "ic_settings")
-}
-}
+	func pageImageIcon() -> UIImage {
+		switch self {
+		case .home:
+			return #imageLiteral(resourceName: "ic_home")
+		case .favorites:
+			return #imageLiteral(resourceName: "ic_favorite_off")
+		case .settings:
+			return #imageLiteral(resourceName: "ic_settings")
+		}
+	}
 
-func pageOrderNumber() -> Int {
-switch self {
-case .home:
-return 0
-case .favorites:
-return 1
-case .settings:
-return 2
-}
-}
+	func pageOrderNumber() -> Int {
+		switch self {
+		case .home:
+			return 0
+		case .favorites:
+			return 1
+		case .settings:
+			return 2
+		}
+	}
 }
--- SeLoger/Infrastructure/Network/APIRequest/HousingRequest.swift
+++ SeLoger/Infrastructure/Network/APIRequest/HousingRequest.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 class HousingRequest: APIRequest {
-var method = RequestType.GET
-var path = "listings"
-var parameters = [String: String]()
-var header: [String: String] =  [
-Constants.HttpHeaderField
-   .acceptType.rawValue: Constants.ContentType.json.rawValue,
-Constants.HttpHeaderField
-.contentType.rawValue: Constants.ContentType.json.rawValue
-]
+	var method = RequestType.GET
+	var path = "listings"
+	var parameters = [String: String]()
+	var header: [String: String] =  [
+		Constants.HttpHeaderField
+			.acceptType.rawValue: Constants.ContentType.json.rawValue,
+		Constants.HttpHeaderField
+			.contentType.rawValue: Constants.ContentType.json.rawValue
+	]
 
-init(identifier: Int) {
-self.path = path + "/\(identifier).json"
-   }
+	init(identifier: Int) {
+		self.path = path + "/\(identifier).json"
+	}
 }
